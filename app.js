9879ad5608e9d7e90d78fe89a2d3eedc7703cec2
Daniel Dyer <danielbdyer@gmail.com>
2018-04-04 18:52:33 -0500

    Started implementing Redis store

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
-var app = require("express")();
-const express = require("express");
-var http = require("http").Server(app);
+var express = require("express");
+var http = require("http");
+var app = express();
+var bodyParser = require("body-parser")
 
-// io is socket.io instance
-var io = require("socket.io")(http);
+var server = http.createServer(app);
+var io = require("socket.io")(server);
+var redis = require('socket.io-redis');
+
+var events = require('events')
+
+var conf = {
+  dbPort: '6379',
+  dbHost: '127.0.0.1',
+  dboptions: {},
+  mainroom: 'MainRoom',
+  port: (process.env.PORT || '3000')
+}
+
+server.listen(conf.port, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/static'));
+
+io.adapter(redis({ host: conf.dbHost, port: conf.dbPort }));
+
+var db = require('redis').createClient(conf.dbPort,conf.dbHost);
+
+var logger = new events.EventEmitter();
+logger.on('newEvent', function(event, data) {
+    console.log('%s: %s', event, JSON.stringify(data));
+});
+
+let users = [];
+let activeSockets = [];
 
 // setting up the javascript file as a static resource
 app.use("/static", express.static("public"));
@@ -12,21 +43,41 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", function(socket) {
-  console.log("a user has connected");
+io.sockets.on("connection", socket => {
 
-  socket.on("digitalcrafts2018", function(message) {
-    console.log(message);
-    io.emit("digitalcrafts2018", message);
+  socket.emit('connected', 'Welcome to the chat server');
+  logger.emit('newEvent', 'userConnected', {'socket':socket.id});
+
+  db.hset([socket.id, 'connectionDate', new Date()], redis.print);
+  db.hset([socket.id, 'socketID', socket.id], redis.print);
+  db.hset([socket.id, 'username', 'anonymous'], redis.print);
+
+  socket.join(conf.mainroom)
+
+  logger.emit('newEvent', 'userJoinsRoom', {'socket':socket.id, 'room':conf.mainroom});
+  socket.emit('subscriptionConfirmed', {'room':conf.mainroom});
+
+  var data = {'room':conf.mainroom, 'username':'anonymous', 'msg':'----- Joined the room -----', 'id':socket.id};
+  io.to(conf.mainroom).emit('userJoinsRoom', data);
+
+  socket.on('disconnect', data => {
+    users.splice(users.indexOf(socket.username), 1);
+    io.sockets.emit('get users', users)
+    activeSockets.splice(activeSockets.indexOf(socket), 1);
+
+    console.log("Socket with %s id just left", socket.id)
+    console.log("%s sockets still connected", activeSockets.length);
   });
 
-  /*
-  socket.on('chat message', function(msg){
-    console.log(msg)
-    io.emit('chat message',msg)
-  }) */
-});
+  socket.on("send message", data => {
+    console.log(data);
+    io.emit("send message", {msg: data.msg, user: socket.username});
+  });
 
-http.listen(3000, function() {
-  console.log("listening on *:3000");
+  socket.on("new user", (userData, callback) => {
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    io.sockets.emit('get users', users);
+  })
 });
--- public/js/client.js
+++ public/js/client.js
@@ -1,4 +1,9 @@
-let socket = io();
+var socket = io.connect(window.location.host);
+
+socket.on('connected', function (data) {
+    console.log(data);
+    socket.emit('getUsersInRoom', {'room':'MainRoom'});
+});
 
 let btnSend = document.getElementById("btnSend");
 let chatMessageTextBox = document.getElementById("chatMessageTextBox");
@@ -7,33 +12,36 @@ let chatMessagesList = document.getElementById("chatMessagesList");
 btnSend.addEventListener("click", function() {
   let message = chatMessageTextBox.value;
   // emit means sending to the channel/room
-  socket.emit("digitalcrafts2018", message);
+  socket.emit("send message", message);
+});
+
+
+socket.on('newMessage', function (data) {
+    console.log("newMessage: %s", JSON.stringify(data));
+    addMessage(data);
+
+    // Scroll down room messages
+    var room_messages = '#room_messages_'+data.room;
+    $(room_messages).animate({
+        scrollTop: $(room_messages).prop('scrollHeight')
+    }, 300);
 });
 
 // listening to that channel
-socket.on("digitalcrafts2018", function(message) {
+socket.on("send message", function(message) {
+  console.log(message)
   let chatMessageLI = document.createElement("li");
-  chatMessageLI.innerHTML = message;
+  chatMessageLI.innerHTML = JSON.stringify(message);
   chatMessagesList.appendChild(chatMessageLI);
 });
 
-/*
-let btnSend = document.getElementById("btnSend")
-btnSend.addEventListener('click',function(){
-  let chatMessage = chatMessageTextBox.value
-  socket.emit('chat message',chatMessage)
-})
+socket.on('disconnect', function (data) {
+    var info = {'room':'MainRoom', 'username':'ServerBot', 'msg':'----- Lost connection to server -----'};
+    addMessage(info);
+});
 
-
-socket.on('chat message',function(message){
-
-  let li = document.createElement("li")
-  li.innerHTML = message
-  chatMessages.appendChild(li)
-
-})
-
-// update users count
-socket.on('update users count',function(count){
-  totalUsersCountHeading.innerHTML = count
-}) */
+// Reconnected to server
+socket.on('reconnect', function (data) {
+    var info = {'room':'MainRoom', 'username':'ServerBot', 'msg':'----- Reconnected to server -----'};
+    addMessage(info);
+});
